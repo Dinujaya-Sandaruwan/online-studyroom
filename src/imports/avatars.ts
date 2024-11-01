779044213a8fdae9d4494a942585868b3a62578c
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2024-11-01 18:42:20 +0530

    Build: Imported avatars to the project

---
+++ src/imports/avatars.ts
@@ -0,0 +1,43 @@
+import raneesha from "../assets/images/raneesha.jpg";
+import ravindu from "../assets/images/ravindu.jpg";
+import gimhan from "../assets/images/gimhan.jpg";
+import hasini from "../assets/images/hasini.jpg";
+import matheesha from "../assets/images/matheesha.jpg";
+import pamodi from "../assets/images/pamodi.jpg";
+import uditha from "../assets/images/uditha.jpg";
+import prabhath from "../assets/images/prabhath.jpg";
+import ahamed from "../assets/images/ahamed.jpg";
+import akash from "../assets/images/akash.jpg";
+import aniththara from "../assets/images/anuththara.jpg";
+import chanduni from "../assets/images/chanduni.jpg";
+import charith from "../assets/images/charith.jpg";
+import chathura from "../assets/images/chathura.jpg";
+import hasitha from "../assets/images/hasitha.jpg";
+import hemal from "../assets/images/Hemal.jpg";
+import heshan from "../assets/images/heshan.jpg";
+import heshani from "../assets/images/heshani.jpg";
+import jayani from "../assets/images/jayani.jpg";
+import nipuna from "../assets/images/nipuna.jpg";
+
+export default {
+  raneesha,
+  ravindu,
+  gimhan,
+  hasini,
+  matheesha,
+  pamodi,
+  uditha,
+  prabhath,
+  ahamed,
+  akash,
+  aniththara,
+  chanduni,
+  charith,
+  chathura,
+  hasitha,
+  hemal,
+  heshan,
+  heshani,
+  jayani,
+  nipuna,
+};
